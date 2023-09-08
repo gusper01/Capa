e5312a95ad4e93cac79e6092bf05a0914b5707e2
gusper01 <gusper01@gmail.com>
2023-09-08 07:39:23 -0300

    Add files via upload

---
+++ Python/trivia.py
@@ -0,0 +1,17 @@
+curso = "Curso de Análisis de Datos - BCRA"
+print(curso[21:26])
+# Cuál de las siguientes opciones es la salida de la ejecución de este sentencia print?
+
+# "Curso de Análisis"
+# "Datos - BCRA"
+# "Datos"
+# "Curso"
+
+curso = "Curso de Análisis de Datos - BCRA"
+print(curso[4::-1])
+# Cuál de las siguientes opciones es la salida de la ejecución de este sentencia print?
+
+# "de An"
+# "osruC"
+# "Datos"
+# "Error"
